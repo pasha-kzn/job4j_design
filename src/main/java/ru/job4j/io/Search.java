14b0658cc0e948a37c28d2c480440b49c782d828
pasha_kzn <pavelmatweeff@yandex.ru>
2025-06-03 06:28:51 +0300

    12. Валидация параметров запуска [#246865 #545030]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,11 +1,14 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
+
+import static java.lang.String.format;
 
 public class Search {
     public static void main(String[] args) throws IOException {
@@ -29,6 +32,16 @@ public class Search {
         }
         if (args.length > 2) {
             throw new IllegalArgumentException("Удалите лишние параметры");
+        }
+        File directory = new File(args[0]);
+        if (!directory.exists()) {
+            throw new IllegalArgumentException(format("Директория не существует: %s", directory.getAbsoluteFile()));
+        }
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(format("Это не директория: %s", directory.getAbsoluteFile()));
+        }
+        if (!(args[1].startsWith(".") && args[1].length() > 1)) {
+            throw new IllegalArgumentException(format("Второй параметр '%s' не является расширением файла", args[1]));
         }
     }
 }
