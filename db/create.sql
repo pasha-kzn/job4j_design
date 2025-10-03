e74c77aeaab566433a15438f90200af18110d5ea
Pavel Matveev KZN <pmatveev@dpd.ru>
2025-10-03 15:37:34 +0300

    2. Схема прав: пользователи и роли. [#1093 #552454]

--- db/create.sql
+++ db/create.sql
@@ -10,16 +10,26 @@ email varchar(63),
 role_id int references roles(id)
 )
 
+create table rules(
+id serial primary key,
+name varchar(63),
+description text
+)
+
 create table roles_rules(
 id serial primary key,
 role_id int references roles(id),
 rule_id int references rules(id)
 )
 
-create table rules(
+create table categories(
 id serial primary key,
-name varchar(63),
-description text
+name varchar(127)
+)
+
+create table states(
+id serial primary key,
+name varchar(63)
 )
 
 create table items(
@@ -40,14 +50,4 @@ create  table attachs(
 id serial primary key,
 attach_url varchar(127),
 item_id int references items(id)
-)
-
-create table categories(
-id serial primary key,
-name varchar(127)
-)
-
-create table states(
-id serial primary key,
-name varchar(63)
 )
