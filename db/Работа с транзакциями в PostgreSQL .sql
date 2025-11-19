ca2eb85fbe3cb2b478e2830164c9a46472afbe36
Pavel Matveev KZN <pmatveev@dpd.ru>
2025-11-20 01:01:15 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813] - добавил точку сохранения

--- db/Работа с транзакциями в PostgreSQL .sql
+++ db/Работа с транзакциями в PostgreSQL .sql
@@ -7,7 +7,7 @@ CREATE TABLE products1
     price    INTEGER
 );
 
-delete from products1;
+delete from products1 where id = 15;
 
 INSERT INTO products1 (name, producer, count, price)
 VALUES ('product_1', 'producer_1', 3, 50);
@@ -32,8 +32,12 @@ SELECT * FROM products1;
 BEGIN TRANSACTION;
 INSERT INTO products1 (name, producer, count, price) VALUES ('product_5', 'producer_5', 17, 45);
 SAVEPOINT first_savepoint;
+INSERT INTO products1 (name, producer, count, price) VALUES ('product_6', 'producer_6', 18, 46);
+SAVEPOINT second_savepoint;
 DELETE FROM products1 WHERE price = 115;
 UPDATE products1 SET price = 75 WHERE name = 'product_1';
+SELECT * FROM products1;
+ROLLBACK TO second_savepoint;
 SELECT * FROM products1;
 ROLLBACK TO first_savepoint;
 SELECT * FROM products1;
