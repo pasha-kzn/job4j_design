1e9258bdf76658461290c83a8f4197b6d6e4e576
Pavel Matveev KZN <pmatveev@dpd.ru>
2026-01-22 16:42:47 +0300

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,25 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String path = "./src/main/resources/app.properties";
+        Config config = new Config(path);
+        config.load();
+        Class.forName(config.value("db.driver"));
+        String url = config.value("db.url");
+        String login = config.value("db.login");
+        String password = config.value("db.password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
